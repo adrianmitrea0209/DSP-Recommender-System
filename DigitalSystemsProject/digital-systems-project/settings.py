141ed55cd0a0a9891978585da6c40d069d36ccc0
adrianmitrea0209 <122695879+adrianmitrea0209@users.noreply.github.com>
2023-04-30 08:56:19 +0100

    Docker Container Set Up

--- DigitalSystemsProject/digital-systems-project/settings.py
+++ DigitalSystemsProject/digital-systems-project/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'digital-systems-project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'digital-systems-project-db',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin0209',
-#         'HOST': 'db',
-#         'PORT': '3306',
-#     }
-# }
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+#  }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'digital-systems-project-db',
+        'USER': 'admin',
+        'PASSWORD': 'admin0209',
+        'HOST': 'db',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
